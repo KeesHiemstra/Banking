48fc80b6a8147b89258a87bbde9f317287e6d426
Kees Hiemstra <chi@xs4all.nl>
2020-02-07 19:47:35 +0100

    WIP Develop_MissedTalliesView

---
+++ Develop MissedTalliesView/ViewModels/MissedTalliesViewModel.cs
@@ -0,0 +1,63 @@
+﻿using CHi.Extensions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Develop_MissedTalliesView.ViewModels
+{
+  public class MissedTalliesViewModel
+  {
+
+    #region [ Fields ]
+
+    Window Parent;
+    //ToDo: Get is from Banking
+    string MissedTalliesJsonFile = "%OneDrive%\\Data\\Banking\\MissedTallies.json".TranslatePath();
+
+    #endregion
+
+    #region [ Properties ]
+
+    public Dictionary<string, string> MissedTallies { get; set; } = new Dictionary<string, string>();
+
+    #endregion
+
+    #region [ Constructor ] 
+    public MissedTalliesViewModel(Window view)
+    {
+
+      Parent = view;
+      if (File.Exists(MissedTalliesJsonFile))
+      {
+        using (StreamReader stream = File.OpenText(MissedTalliesJsonFile))
+        {
+          string json = stream.ReadToEnd();
+          MissedTallies = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+        }
+      }
+      view.DataContext = this;
+
+    }
+
+    #endregion
+
+    public void ShowMissedTallies()
+    {
+
+      MissedTalliesWindow missedTalliesView = new MissedTalliesWindow()
+      {
+        Left = Parent.Left + 20,
+        Top = Parent.Top + 20
+      };
+      missedTalliesView.MissedTalliesListBox.ItemsSource = MissedTallies;
+      missedTalliesView.ShowDialog();
+
+    }
+
+  }
+}
